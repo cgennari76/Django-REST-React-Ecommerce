28cc6ac0466eb82ad412482476926201b74e66a4
cgennari76 <31255261+cgennari76@users.noreply.github.com>
2022-04-04 19:03:33 -0700

    fixed serializer in views

--- backend/base/views.py
+++ backend/base/views.py
@@ -32,14 +32,10 @@ def getRoutes(request):
 def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
-    return Response(serializer)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
